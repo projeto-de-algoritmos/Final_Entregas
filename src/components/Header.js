23868ef92cab00f1e61657ae9cb3ca671022b74e
Ítalo Alves <italooko@gmail.com>
2021-05-07 22:45:38 -0300

    Adicionando Header, SideBar e tela Armazem

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { } from 'react'
+
+const Header = () => {
+  return (
+    <div className="row">
+              
+      <header>
+        <div className="col-md-5">
+          <nav className="navbar-default pull-left">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation" id="nav-toggle" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+            </div>
+          </nav>
+          <div className="search">
+            <div className="header-top">
+              <button className="btn btn-danger">Novo Produto</button>
+            </div>
+          </div>
+        </div>
+      </header>
+               
+    </div>
+  )
+}
+
+export default Header
